8f1e3574ed6d0b4fc9fa027b3cea07cdebac26ed
dan <parshall.dan@gmail.com>
2021-06-22 21:01:35 -0400

    init cookiecutter datascience

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building a directed graph of family relationships from name data',
+    author='Dan Parshall',
+    license='',
+)
