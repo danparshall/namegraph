8ee9fa5ece0e74ce42b3878f104c58a522f5529b
Superpapocho <juan.russy@urosario.edu.co>
2021-12-03 14:19:45 -0500

    test_dataset.py 1.0

--- src/data/cleanup.py
+++ src/data/cleanup.py
@@ -261,7 +261,25 @@ def fix_nombre(nombre):
         nombre = "-".join(nombre.split("- "))
     return nombre
 
+def test_data(org_data, test_data_path, dtypes):
+    test = pd.read_csv(test_data_path, sep = '\t', dtype = dtypes, keep_default_na=False, na_values = nan_values)
+    print("Test",test_data_path)
 
+    for row in test.to_dict(orient='records'):
+        try:
+            for key in row.keys():
+                if pd.isnull(row[key]):
+                    row[key] = ''
+            res = org_data[org_data.cedula == row['cedula']].iloc[0].to_dict()
+        except IndexError:
+            print(row['cedula'], ": NOT IN THIS SUBFRAME")
+            continue
+        if res == row:
+            print(row['cedula'], ": OK")
+        else:
+            print(row['cedula'], ": FAILED")
+            print(row)
+            print(res)
 
 def load_registry(filepath_raw, logger, N_ROWS=None):
 
@@ -273,7 +291,7 @@ def load_registry(filepath_raw, logger, N_ROWS=None):
                   'ced_spouse': str, 'ced_padre': str, 'ced_madre': str,
                   'is_nat':bool, 'is_nat_padre':bool, 'is_nat_madre':bool
                   }
-    rf = pd.read_csv(filepath_raw, sep='\t', encoding='latin',
+    rf = pd.read_csv(filepath_raw, sep='\t', encoding='utf-8',
                      parse_dates=date_cols, dtype=dtypes_reg,
                      keep_default_na=False, na_values=nan_values,
                      nrows=N_ROWS,
---
+++ src/data/test_dataset.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import click
+import logging
+from pathlib import Path
+#from dotenv import find_dotenv, load_dotenv
+
+# load repo files
+import cleanup
+import extract
+
+from importlib import reload
+reload(cleanup)
+reload(extract)
+
+
+# %run make_dataset.py "../../data/raw/SAMP_100k.tsv" '../../data/interim/'
+
+date_cols = ['dt_birth', 'dt_death', 'dt_marriage']
+
+dtypes_namedata = { 'cedula': str, 'nombre': str, 'nombre_spouse': str, 'marital_status': str,
+                    'place_birth': str, 'nombre_padre': str, 'nombre_madre': str, 
+                    'ced_spouse': str, 'ced_padre': str, 'ced_madre': str,
+                    'is_nat': bool, 'is_nat_padre': bool, 'is_nat_madre': bool,
+                    }
+
+@click.command()
+@click.argument('filepath_raw', type=click.Path(exists=True))
+@click.argument('folder_interim', type=click.Path())
+def main(filepath_raw, folder_interim):
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in ../interim).
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('making interim data set from raw data')
+
+    ## STUFF FROM NB 1.0
+    print("Loading registry")
+    rf = cleanup.load_registry(filepath_raw, logger)
+
+    print("Cleaning registry")
+    rf = cleanup.clean_nombres(rf, folder_interim)
+
+    cleanup.test_data(rf, '../../data/simpsons_test_cases.tsv', dtypes_namedata)
+
+    ## BEGIN NB 2.0
+    print("Parsing rows to extract surnames")
+    surnames_extracted = rf.progress_apply(lambda row: extract.parse_fullrow(row), axis=1, result_type='expand')
+
+
+
+    ## BEGIN NB 3.0
+
+
+
+    ## BEGIN 4.0
+
+
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+#    load_dotenv(find_dotenv())
+
+    main()
