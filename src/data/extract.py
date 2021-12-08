1f15f63920af417676a42692e58887b391980d7b
dan <parshall.dan@gmail.com>
2021-12-08 14:57:18 -0500

    split out madre/padre parsing into subfuncs

--- src/data/extract.py
+++ src/data/extract.py
@@ -49,6 +49,125 @@ def get_substrings(nombre, START=0):
                 yield sub
 
 
+
+def parse_padre(row, parts, nomset, pset):
+    # start by trying the first LEN-1 tokens as a single name, then LEN-2 tokens, etc
+    # this matches longest chunk found, so it should pick up compound names like DE LA CUEVA
+    if row.nombre_padre:
+        
+        # we try names that might have doubling first, befor moving to the more common situation
+        poss_padre = check_nombre_doubling(row.nombre_padre)
+        poss_pset = set(poss_padre.split())
+        
+        if (poss_padre
+            and (poss_padre in row.nombre)
+            and not row.nombre.endswith(poss_padre)
+            and poss_pset.issubset(nomset)
+            ):
+            padre = poss_padre
+            parts = ''.join(row.nombre.split(padre, maxsplit=1)).strip().split()
+            
+        else:
+            # start by trying everything except the last element (always a prename), and work down
+            for ind in range(len(parts)-1, 0, -1):
+                guess = ' '.join(parts[:ind])
+                poss_pset = set(guess.split())
+                if (guess in row.nombre_padre
+                    and poss_pset.issubset(nomset)
+                    and poss_pset.issubset(pset)
+                    ):
+                    # update before checking mother's name
+                    padre = guess
+                    parts = row.nombre.split(padre, maxsplit=1)[1].split()
+                    break
+    else:
+        padre = ""
+    return padre, parts
+
+
+
+def parse_madre(row, parts, nomset, mset):
+    if row.nombre_madre:
+        poss_madre = check_nombre_doubling(row.nombre_madre)
+        poss_mset = set(poss_madre.split())
+        if (poss_madre
+            and (poss_madre in row.nombre)
+            and not row.nombre.endswith(poss_madre)
+                and poss_mset.issubset(nomset) and poss_mset.issubset(mset)):
+            madre = poss_madre
+        elif not madre:
+            nombre_madre = row.nombre_madre
+
+            if nombre_madre.startswith(parts[0]):
+            # try to remove any catholic addons from both citizen and mother
+            # this isn't a concern when in social form
+            # complicated bc surnames like "GOMEZ DE LA TORRE" mean we have to skip the zeroth token
+
+                # if names have underscores
+                m_de_pre_nombre = re_de_pre_UNDERSCORE.match(
+                    ' '.join(parts[1:]))
+                if m_de_pre_nombre:
+                    # keep 'parts' as a list
+                    parts = parts[:1] + \
+                        m_de_pre_nombre.group(1).split()
+
+                # names without underscores
+                mom_parts = nombre_madre.split()
+                m_de_pre_madre = re_de_pre.match(
+                    ' '.join(mom_parts[1:]))
+                if m_de_pre_madre:
+                    # keep 'nombre_madre' as string
+                    nombre_madre = mom_parts[0] + \
+                        " " + m_de_pre_madre.group(1)
+
+            for ind in range(len(parts)-1, 0, -1):
+                guess = ' '.join(parts[:ind])
+                poss_mset = set(guess.split())
+                if ((guess in nombre_madre)
+                    and not row.nombre.endswith(guess)
+                        and poss_mset.issubset(nomset) and poss_mset.issubset(mset)):
+                    # now check which is the better fit
+                    if (guess == nombre_madre):
+                        # when madre is in short legal form and daughter has the same prename1
+                        # it can look like the mother's surname is "GONZALEZ MARIA", etc.
+                        # So ignore these, ??? because we need to handle them later
+                        pass
+                    else:
+                        madre = guess
+                        break
+        else:
+            madre = ""
+    return madre
+
+
+
+def parse_overlaps(row, nomset, pset, mset):
+    surname = check_nombre_doubling(row.nombre)
+    if surname != "":
+        madre = surname
+        padre = surname
+    else:
+        padre = ""
+        madre = ""
+        for guess in get_substrings(row.nombre_madre.split()):
+            if (guess in row.nombre
+                and not row.nombre.endswith(guess)
+               ):
+                poss_padre = row.nombre.split(guess)[0].strip()
+                poss_set = set(poss_padre.split())
+                guess_set = set(guess.split())
+                if (poss_padre in row.nombre_padre
+                    and not row.nombre.endswith(poss_padre)
+                    and poss_set.issubset(nomset) and poss_set.issubset(pset)
+                    and guess_set.issubset(nomset) and guess_set.issubset(mset)
+                    ):
+                    padre = poss_padre
+                    madre = guess
+                    break
+    return padre, madre
+
+
+
 re_de_pre = re.compile(r"(^.*)\s(DEL?\s\w+.*$)")
 re_de_pre_UNDERSCORE = re.compile(r"(^.*)\s(DEL?_\w+.*$)")
 def parse_fullrow(row):
@@ -62,72 +181,26 @@ def parse_fullrow(row):
     if not row.nombre_padre and not row.nombre_madre:
         return out
 
-    nombre = row.nombre
-    nomset = set(nombre.split())
-    madre = ""
-    padre = ""
-    prenames = ""
-
     # check if madre/padre have overlapping tokens (requires special handling)
+    nomset = set(row.nombre.split())
     mset = set(row.nombre_madre.split())
     pset = set(row.nombre_padre.split())
     both = pset & mset
     flag_overlap = len(both) > 0
 
     if flag_overlap:
-        surname = check_nombre_doubling(nombre)
-        if surname != "":
-            madre = surname
-            padre = surname
-        else:
-            for guess in get_substrings(row.nombre_madre.split()):
-                if (guess in nombre
-                        and not nombre.endswith(guess)):
-                    poss_padre = nombre.split(guess)[0].strip()
-                    poss_set = set(poss_padre.split())
-                    guess_set = set(guess.split())
-                    if (poss_padre in row.nombre_padre
-                        and not nombre.endswith(poss_padre)
-                        and poss_set.issubset(nomset) and poss_set.issubset(pset)
-                        and guess_set.issubset(nomset) and guess_set.issubset(mset)
-                        ):
-                        padre = poss_padre
-                        madre = guess
-                        break
+        padre, madre = parse_overlaps(row, nomset, pset, mset)
+    else:
+        madre = ""
+        padre = ""
 
     # if the overlap method wasn't successful, parse father/mother surname separately
     if not madre and not padre:
-        parts = nombre.split()
+        parts = row.nombre.split()
 
         #### FATHERS NAME ####
-        # start by trying the first LEN-1 tokens as a single name, then LEN-2 tokens, etc
-        # this matches longest chunk found, so it should pick up compound names like DE LA CUEVA
         try:
-            if row.nombre_padre:
-                
-                # we try names that might have doubling first, befor moving to the more common situation
-                poss_padre = check_nombre_doubling(row.nombre_padre)
-                poss_pset = set(poss_padre.split())
-                if (poss_padre
-                    and (poss_padre in row.nombre)
-                    and not nombre.endswith(poss_padre)
-                        and poss_pset.issubset(nomset)):
-                    padre = poss_padre
-                    parts = ''.join(nombre.split(
-                        padre, maxsplit=1)).strip().split()
-                    
-                else:
-                    # start by trying everything except the last element (always a prename), and work down
-                    for ind in range(len(parts)-1, 0, -1):
-                        guess = ' '.join(parts[:ind])
-                        poss_pset = set(guess.split())
-                        if ((guess in row.nombre_padre)
-                                    and poss_pset.issubset(nomset) and poss_pset.issubset(pset)
-                                ):
-                            # update before checking mother's name
-                            padre = guess
-                            parts = nombre.split(padre, maxsplit=1)[1].split()
-                            break
+            padre, parts = parse_padre(row, parts, nomset, pset)
         except:
             out['sur_padre'] = "WTF PADRE PROBLEM"
             return out
@@ -135,55 +208,7 @@ def parse_fullrow(row):
         #### MOTHERS NAME ####
         # having removed the padre name from the front of the string, try similar trick with the madre name
         try:
-            if row.nombre_madre:
-                poss_madre = check_nombre_doubling(row.nombre_madre)
-                poss_mset = set(poss_madre.split())
-                if (poss_madre
-                    and (poss_madre in row.nombre)
-                    and not nombre.endswith(poss_madre)
-                        and poss_mset.issubset(nomset) and poss_mset.issubset(mset)):
-                    madre = poss_madre
-                elif not madre:
-                    nombre_madre = row.nombre_madre
-
-                    if nombre_madre.startswith(parts[0]):
-                    # try to remove any catholic addons from both citizen and mother
-                    # this isn't a concern when in social form
-                    # complicated bc surnames like "GOMEZ DE LA TORRE" mean we have to skip the zeroth token
-                        
-                        # if names have underscores
-                        m_de_pre_nombre = re_de_pre_UNDERSCORE.match(
-                            ' '.join(parts[1:]))
-                        if m_de_pre_nombre:
-                            # keep 'parts' as a list
-                            parts = parts[:1] + \
-                                m_de_pre_nombre.group(1).split()
-
-                        # names without underscores
-                        mom_parts = nombre_madre.split()
-                        m_de_pre_madre = re_de_pre.match(
-                            ' '.join(mom_parts[1:]))
-                        if m_de_pre_madre:
-                            # keep 'nombre_madre' as string
-                            nombre_madre = mom_parts[0] + \
-                                " " + m_de_pre_madre.group(1)
-
-                    for ind in range(len(parts)-1, 0, -1):
-                        guess = ' '.join(parts[:ind])
-                        poss_mset = set(guess.split())
-                        if ((guess in nombre_madre)
-                            and not nombre.endswith(guess)
-                                and poss_mset.issubset(nomset) and poss_mset.issubset(mset)):
-                            # now check which is the better fit
-                            if (guess == nombre_madre):
-                                # when madre is in short legal form and daughter has the same prename1
-                                # it can look like the mother's surname is "GONZALEZ MARIA", etc.
-                                # So ignore these, ??? because we need to handle them later
-                                pass
-                            else:
-                                madre = guess
-                                break
-
+            madre = parse_madre(row, parts, nomset, mset)
         except:
             out['sur_madre'] = "WTF MADRE PROBLEM"
             return out
@@ -210,15 +235,10 @@ def parse_fullrow(row):
 
 
 
-
 # There are plenty of prenames which have some sort of honorific(e.g. "DEL CARMEN").  The goal here is to find any of those, and render them into a single token(e.g. "DEL_CARMEN").
-
 # By definition, this only applies to names with at least 3 tokens(since the honorific is minimum of 2, and the prename is a minimum of 1).
-
 # Occasionally, someone will have something like "SANTA DEL CARMEN".  In practice, they're referred to as "CARMEN".
-
 # Because of cleaning in NB 1.0, this section has almost nothing to catch.
-
 # in all cases, we look for a word boundary as the first group, then our funky name as the second
 
 re_von = re.compile(u"(\s)(V[AO]N \w{2,})(\s|$)")              # these results are subset of "re_vande"
