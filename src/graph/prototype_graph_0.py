7496ba38c51f49a3080db9ba32ab34d4ef9549b6
Superpapocho <juan.russy@urosario.edu.co>
2022-05-06 09:18:59 -0500

    Graph configuration reading
    EdgeList file do not uses continuous node ids and
    is created as an undirected graph

--- src/graph/prototype_graph_0.py
+++ src/graph/prototype_graph_0.py
@@ -1,10 +1,15 @@
-import pathlib
+import time
 import networkit as nk
-dir_name = '/home/juan.russy/shared/proof_run_FamNet/interim/'
+dir_name = ''
 base_filename = 'PROOF_GRAPH_matched_exact_name.txt'
 filename = dir_name + base_filename
 print(filename)
 
+startTime = time.time()
+
 nk.engineering.setNumberOfThreads(2)
-g = nk.graphio.EdgeListReader(separator = '\t', firstNode = 0, continuous = True, directed = True).read(filename)
+g = nk.graphio.EdgeListReader(separator = '\t', firstNode = 0, continuous = False, directed = False).read(filename)
 nk.graphio.writeGraph(g, dir_name + "example.gml", nk.Format.GML)
+
+executionTime = (time.time() - startTime)
+print('Execution time in seconds: ' + str(executionTime))
