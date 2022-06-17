68a3e9875807af5c674763a3c6510aeeb243ad3f
Juan Andres Russy <70037131+juan-andres-russy@users.noreply.github.com>
2022-06-17 09:26:20 -0500

    EdgeListo to GML (Graph format)

--- src/graph/edgelist_to_gml.py
+++ src/graph/edgelist_to_gml.py
@@ -1,15 +1,28 @@
 import time
 import networkit as nk
 dir_name = ''
-base_filename = 'PROOF_GRAPH_matched_exact_name.txt'
+base_filename = 'Matched_Graph.txt'
+output_filename = 'example.gml'
 filename = dir_name + base_filename
 print(filename)
 
-startTime = time.time()
-
+#%% EdgeListReader method
+# EdgeListReader -> Execution time in seconds: 154.35551619529724
+# writeGraph -> Execution time in seconds: 20.2667133808136
 nk.engineering.setNumberOfThreads(2)
+startTime = time.time()
 g = nk.graphio.EdgeListReader(separator = '\t', firstNode = 0, continuous = False, directed = False).read(filename)
-nk.graphio.writeGraph(g, dir_name + "example.gml", nk.Format.GML)
-
 executionTime = (time.time() - startTime)
-print('Execution time in seconds: ' + str(executionTime))
+print('EdgeListReader -> Execution time in seconds: ' + str(executionTime))
+
+# startTime = time.time()
+# nk.graphio.writeGraph(g, dir_name + output_filename, nk.Format.GML)
+# executionTime = (time.time() - startTime)
+# print('writeGraph -> Execution time in seconds: ' + str(executionTime))
+
+#%% GMLGraphReader method
+# Execution time in seconds: 53.78959369659424
+# startTime = time.time()
+# g = nk.graphio.GMLGraphReader().read(dir_name + output_filename)
+# executionTime = (time.time() - startTime)
+# print('GMLGraphReader -> Execution time in seconds: ' + str(executionTime))
